78a49ad1e988722d355b15420a01a957353b7b5e
divyjainjd <divy.jain@bombaysoftwares.com>
2023-07-05 12:16:30 +0530

    Update setup.py and made new build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='bombaysoftwares-pysupp',
     packages=find_packages(),
     version='1.0.0',
-    description='The bombaysoftwares-pysupp package provides a comprehensive set of utility functions for various operations in Python. These functions simplify common tasks such as date formatting, timestamp conversion,  manipulating strings. This package is designed to enhance the functionality of various operations in your Python projects.',
+    description='The bombaysoftwares-pysupp package provides a comprehensive set of utility functions for various operations in Python. These functions simplify common tasks such as date formatting, timestamp conversion, manipulating strings. This package is designed to enhance the functionality of various operations in your Python projects.',
     author='Bombay Softwares',
     long_description="long_description",
     long_description_content_type='text/markdown',
